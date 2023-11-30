324b36e874e174b99bc4a383b48f19acb6307aca
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-11-30 23:29:40 +0100

    Add files via upload

---
+++ OOP 30.11.2023/OOP 30.11.2023/OOP 30.11.2023.cpp
@@ -0,0 +1,176 @@
+﻿#include <iostream>
+using namespace std;
+
+class Malika {
+public:
+	string surname;
+	string adress;
+	unsigned short age;
+	unsigned short weight;
+	string hair_color;
+	string eye_color;
+	unsigned short sleight_of_hand;
+	unsigned short number_of_legs;
+	unsigned short number_of_fingers;
+	unsigned short number_of_toes;
+	unsigned short number_of_teeth;
+	unsigned short leg_size;
+
+	void Sleep()
+	{
+		cout << "Zzzz..."<<"\n";
+	}
+	void Eat()
+	{
+		cout << "maybe I'll go get something to eat)" << "\n";
+	}
+	void Speak()
+	{
+		cout << "Bla bla bla..."<<"\n";
+	}
+	void Read()
+	{
+		cout << "I like to read code)" << "\n";
+	}
+	void PlayComputerGames()
+	{
+		cout << "I hate my teammates!!!!!!!" << "\n";
+	}
+};
+
+class AirАreshener {
+public:
+	string brand;
+	string form;
+	unsigned short number_of_connectors;
+	unsigned short number_of_buttons;
+	string cylinder_type;
+	double working_time;
+
+	void Spritz()
+	{
+		cout << "Pshhh" << "\n";
+	}
+	void Вischarge()
+	{
+		cout << "Hey, I'm out of battery" << "\n";
+	}
+	void Sound()
+	{
+		cout << "I make a sound, so don't be scared at night)))))" << "\n";
+	}
+	void Break()
+	{
+		cout << "I'm sorry, I broke down!" << "\n";
+	}
+	void ExhaustBallon()
+	{
+		cout << "I used the whole bottle with the smell." << "\n";
+	}
+};
+
+class Fridge {
+public:
+	string brand;
+	string color;
+	unsigned short height;
+	unsigned short width;
+	unsigned short number_of_compartments;
+	unsigned short volume_in_liters;
+	unsigned short maximum_temperature;
+	unsigned short minimum_temperature;
+	string connection_method;
+	bool light;
+
+	void StoreFood()
+	{
+		cout << "I can store food" << "\n";
+	}
+	void MakeSound()
+	{
+		cout << "I can make a sound" << "\n";
+	}
+	void Freeze()
+	{
+		cout << "I can lower the temperature of food" << "\n";
+	}
+	void ShutDown()
+	{
+		cout << "If the lights go out, so do I(((((" << "\n";
+	}
+	void Stagger()
+	{
+		cout << "If you don't close the freezer tightly, get the mop ready)" << "\n";
+	}
+};
+
+class Cat {
+public: 
+	string name;
+	unsigned short age;
+	string coat_color;
+	unsigned short weight;
+	string breed;
+	unsigned short number_of_paws;
+
+	void Sound()
+	{
+		cout << "Sound meow meow meow" << "\n";
+	}
+	void Play()
+	{
+		cout << "Play with me!" << "\n";
+	}
+	void Eat()
+	{
+		cout << "I want to eat meow" << "\n";
+	}
+	void Pee()
+	{
+		cout << "I have to go to the bathroom" << "\n";
+	}
+	void Attention()
+	{
+		cout << "caress me." << "\n";
+	}
+};
+
+class Grill {
+public:
+	string brand;
+	unsigned short max_temperature;
+	unsigned short min_temperature;
+	unsigned short height;
+	unsigned short width;
+	unsigned short number_of_buttons;
+	string connection_method;
+
+	void Cook()
+	{
+		cout << "I can cook a meal" << "\n";
+	}
+	void Dirt()
+	{
+		cout << "I can get dirty" << "\n";
+	}
+	void ShutDown()
+	{
+		cout << "I can turn myself off if the lights go out." << "\n";
+	}
+	void Overheating()
+	{
+		cout << "If I'm on for too long, I'll overheat." << "\n";
+	}
+	void OverheatFood()
+	{
+		cout << "if you don't watch the food, it'll burn." << "\n";
+	}
+};
+
+int main()
+{
+
+
+
+
+}
